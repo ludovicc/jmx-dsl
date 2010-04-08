3b202299171bc27688660d8490072293e96413f6
Ludovic Claude <lclaude@spi-lclaude-linux.(none)>
2010-04-08 18:47:22 +0200

    * Fix configuration of RRD bases * Add forEach verb to DSL to iterate over jmx beans when creating categories

---
+++ src/main/groovy/info/kartikshah/jmx/dsl/engine/ForEachBeanDelegate.groovy
@@ -0,0 +1,13 @@
+package info.kartikshah.jmx.dsl.engine
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lclaude
+ * Date: 8 avr. 2010
+ * Time: 15:02:38
+ * To change this template use File | Settings | File Templates.
+ */
+class ForEachBeanDelegate {
+  def ForEachBeanDelegate(java.lang.Object o) {
+  }
+}
